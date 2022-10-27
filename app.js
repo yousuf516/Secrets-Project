33e4d71f0245c58a976f4ac502ca972907d8b8c0
INTEL <yousufzubair1234@gmail.com>
2022-10-27 06:59:14 +0500

    Installed mongoose-encryption

--- app.js
+++ app.js
@@ -8,8 +8,57 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = {
+    email: String,
+    password: String
+}
+
+const User = mongoose.model("User", userSchema);
+
 app.get("/", function(req, res){
-    res.send("Hello");
+    res.render("home");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.post("/register", function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("secrets");
+        }
+    })
+});
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser){
+        if(err){
+            console.log(err)
+        }
+        else if(foundUser){
+            if(foundUser.password === password){
+                res.render("secrets")
+            }
+        }
+    });
 });
 
 var port = 5501;
